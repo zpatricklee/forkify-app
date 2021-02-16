1b45fb0abdf6602f417592c3a4b7eda2c6fadca3
zpatricklee <zpatricklee@gmail.com>
2021-02-16 15:18:28 -0800

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = new (function () {
+  console.log('Welcome to the new application!');
+})();
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -134,5 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
